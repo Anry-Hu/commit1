b17c51c97e582be11d96944ed607029704a56dfb
anry-hu <1046709970@qq.com>
2018-03-20 00:32:39 +0800

    wordtest.c
    单词数测试用例

--- wordtest.c
+++
@@ -1,4 +0,0 @@
-File write,name = new File(outputPath);
-            writename.createNe|wFile();
-            Buffe,redWriter out = new BufferedWriter(new FileWriter(writename));
-            out.close();
